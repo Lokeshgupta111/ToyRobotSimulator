87343b85ed91a85d877dcdadc42f624f7a54973e
Lokesh Gupta <lokeshgupta112@outlook.com>
2025-03-30 15:20:02 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using RobotSimulator;
+
+namespace RobotSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int x, y;
+           
+            Console.Write("Enter an X: ");
+
+            while (!int.TryParse(Console.ReadLine(), out x))
+            {
+                Console.Write("Invalid X! Please enter a valid integer: ");
+            }
+            Console.Write("Enter an Y: ");
+
+            while (!int.TryParse(Console.ReadLine(), out y))
+            {
+                Console.Write("Invalid Y! Please enter a valid integer: ");
+            }
+
+            Console.Write("Enter a Direction for moving Robot: ");
+            string sDirection = Console.ReadLine();
+            sDirection = sDirection.ToUpper();
+            if (!Enum.TryParse(sDirection, true, out Face direction))
+            {
+                Console.WriteLine("Invalid Direction. Please enter a valid Direction.");
+                return;
+            }
+            ToyRobot toy = new ToyRobot();
+
+            if (toy.Place(x, y, direction))
+            {
+                toy.Left();
+               // toy.Move();
+                //toy.Move();
+                //toy.Left();
+                //toy.Move();
+            }
+            toy.Report();
+        }
+    }
+}
---
+++ ToyRobot.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotSimulator
+{
+    public enum Face
+    {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
+    }
+    public class ToyRobot
+    {
+        public int x = 0, y = 0;
+        public Face f;
+
+
+        public bool Place(int x, int y, Face f)
+        {
+            this.x = x;
+            this.y = y;
+            this.f = f;
+            if (x > 5 || x < 0 || y > 5 || y < 0) return false;
+            //Console.WriteLine($"Toy placed at {x}, {y}, {(Face)f}");
+            return true;
+        }
+
+        public void Move()
+        {
+            if (f == Face.EAST && x < 5)
+                x += 1;
+            if (f == Face.WEST && x > 0)
+                x -= 1;
+            if (f == Face.NORTH && y < 5)
+                y += 1;
+            if (f == Face.SOUTH && y > 0)
+                y -= 1;
+
+
+        }
+
+        public void Left()
+        {
+            if (f == (Face)0) f = (Face)3;
+            else f -= 1;
+        }
+
+        public void Right()
+        {
+            if (f == (Face)3) f = (Face)0;
+            else f += 1;
+
+        }
+        public void Report()
+        {
+            if (x > 5 || x < 0 || y > 5 || y < 0) { Console.WriteLine($"Output: Robot is off in the table."); return; }
+            Console.WriteLine($"Output: {x}, {y}, {(Face)f}");
+        }
+    }
+
+}
+
